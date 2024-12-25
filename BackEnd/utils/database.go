bb51c603fab4e8961053d4eeca5602939400a681
Furkan Baran <furkanbarann5@gmail.com>
2024-12-25 12:08:13 +0300

    asd

--- BackEnd/utils/database.go
+++ BackEnd/utils/database.go
@@ -1,4 +1,4 @@
-package main
+package utils
 
 import (
 	"context"
@@ -20,26 +20,23 @@ func DbConnect() *pgx.Conn {
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close(context.Background())
 	return conn
 }
 func CheckUser(conn *pgx.Conn, userName string) int {
 
-	rows, err := conn.Query(context.Background(), "SELECT * FROM users WHERE user_name=$1", userName)
-	fmt.Println(rows)
+	rows, err := conn.Query(context.Background(), "SELECT id,name FROM users WHERE user_name=$1", userName)
 	if err != nil {
-
-		panic(err)
+		log.Fatal(err)
 	}
 	defer rows.Close()
-	// for rows.Next() {
-	// 	var id int32
-	// 	var name string
-	// 	if err := rows.Scan(&id, &name); err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Printf("%d | %s\n", id, name)
-	// }
+	for rows.Next() {
+		var id int32
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			panic(err)
+		}
+		fmt.Printf("%d | %s\n", id, name)
+	}
 
 	return 1
 }
@@ -49,14 +46,15 @@ func createTable(conn *pgx.Conn) {
 		panic(err)
 	}
 }
-func insert(conn *pgx.Conn, userInfo User) {
-	name := userInfo.name
-	id := userInfo.id
-	_, err := conn.Exec(context.Background(), "INSERT INTO Users(id,name)   VALUES ($1,$2);", id, name)
-	if err != nil {
-		panic(err)
-	}
-}
+
+//	func insert(conn *pgx.Conn, userInfo User) {
+//		name := userInfo.name
+//		id := userInfo.id
+//		_, err := conn.Exec(context.Background(), "INSERT INTO Users(id,name)   VALUES ($1,$2);", id, name)
+//		if err != nil {
+//			panic(err)
+//		}
+//	}
 func query(conn *pgx.Conn) {
 	rows, err := conn.Query(context.Background(), "SELECT * FROM users WHERE id=10")
 	if err != nil {
