4d26f2a874f46ae503157841ff15938f514a1134
Furkan Baran <furkanbarann5@gmail.com>
2024-12-25 11:21:33 +0300

    asd

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -12,7 +12,7 @@ type Customer struct {
 	surname     string
 	userName    string
 	phoneNumber string
-	password    string `json:"password"`
+	password    string
 }
 type PostData struct {
 	Title  string `json:"title"`
